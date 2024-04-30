3772b327e6de3d437c63f710b336fadd71bcbfa2
lucas132000 <ld132000@gmail.com>
2024-04-30 11:16:47 -0300

    inserção da barra inferior

--- flutter_application_1/flutter_teste/lib/main.dart
+++ flutter_application_1/flutter_teste/lib/main.dart
@@ -1,142 +1,9 @@
-import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_teste/about_page.dart';
 import 'package:flutter_teste/home_page.dart';
 import 'package:flutter_teste/settings_screen.dart';
-
-class AppBarDemo extends StatelessWidget {
-  const AppBarDemo({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    // Obtenha uma instância de MaterialLocalizations para acessar os textos localizados
-    final MaterialLocalizations localizations =
-        MaterialLocalizations.of(context);
-
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue, // Cor de fundo do AppBar
-        leading: Builder(
-          builder: (context) => IconButton(
-            icon: const Icon(Icons.menu),
-            tooltip: localizations.openAppDrawerTooltip,
-            onPressed: () {
-              // Abre o Drawer ao clicar no ícone do menu
-              Scaffold.of(context).openDrawer();
-            },
-          ),
-        ),
-        title: Text(
-          'AppBar Title', // Altere para a string desejada ou localize a partir de seus próprios recursos
-        ),
-        actions: [
-          IconButton(
-            tooltip:
-                'Favorite', // Altere para a string desejada ou localize a partir de seus próprios recursos
-            icon: const Icon(Icons.favorite),
-            onPressed: () {},
-          ),
-          IconButton(
-            tooltip:
-                'Search', // Altere para a string desejada ou localize a partir de seus próprios recursos
-            icon: const Icon(Icons.search),
-            onPressed: () {},
-          ),
-          PopupMenuButton<String>(
-            itemBuilder: (context) {
-              return [
-                PopupMenuItem(
-                  child: Text(
-                      'First'), // Altere para a string desejada ou localize a partir de seus próprios recursos
-                ),
-                PopupMenuItem(
-                  child: Text(
-                      'Second'), // Altere para a string desejada ou localize a partir de seus próprios recursos
-                ),
-                PopupMenuItem(
-                  child: Text(
-                      'Third'), // Altere para a string desejada ou localize a partir de seus próprios recursos
-                ),
-              ];
-            },
-          ),
-        ],
-      ),
-      body: Center(
-        child: Text(
-          'Home Tab', // Altere para a string desejada ou localize a partir de seus próprios recursos
-        ),
-      ),
-      // Adiciona o Drawer ao Scaffold
-      drawer: Drawer(
-        child: ListView(
-          // Define os itens no Drawer
-          children: [
-            // Adiciona um DrawerHeader com uma foto de perfil arredondada
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Column(
-                children: [
-                  CircleAvatar(
-                    radius:
-                        50, // Define o raio da imagem para torná-la arredondada
-                    backgroundImage: AssetImage('assets/images/profile.jpg'),
-                  ),
-                  Text(
-                    'Profile',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            ListTile(
-              leading: Icon(Icons.home),
-              title: Text('Home'),
-              onTap: () {
-                // Ação ao selecionar "Home"
-                Navigator.pop(context);
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => HomePage()),
-                ); // Fechar o Drawer
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.settings),
-              title: Text('Settings'),
-              onTap: () {
-                // Ação ao selecionar "Settings"
-                Navigator.pop(context);
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => SettingsScreen()),
-                ); // Fechar o Drawer
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.info),
-              title: Text('About'),
-              onTap: () {
-                // Ação ao selecionar "About"
-                Navigator.pop(context);
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => AboutPage()),
-                );  // Fechar o Drawer
-              },
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
 
 void main() {
   runApp(
@@ -155,17 +22,171 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'AppBar Demo',
       localizationsDelegates: const [
-        // Configurações para internacionalização com flutter_localizations
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
       supportedLocales: const [
-        // Lista de idiomas suportados
-        Locale('en', 'US'), // Inglês
-        Locale('pt', 'BR'), // Português do Brasil
+        Locale('en', 'US'),
+        Locale('pt', 'BR'),
       ],
       home: const AppBarDemo(),
+    );
+  }
+}
+
+class AppBarDemo extends StatefulWidget {
+  const AppBarDemo({Key? key}) : super(key: key);
+
+  @override
+  _AppBarDemoState createState() => _AppBarDemoState();
+}
+
+class _AppBarDemoState extends State<AppBarDemo> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text('Comentários'),
+    Text('Agenda'),
+    Text('Conta'),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final MaterialLocalizations localizations =
+        MaterialLocalizations.of(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        leading: Builder(
+          builder: (context) => IconButton(
+            icon: const Icon(Icons.menu),
+            tooltip: localizations.openAppDrawerTooltip,
+            onPressed: () {
+              Scaffold.of(context).openDrawer();
+            },
+          ),
+        ),
+        title: Text(
+          'AppBar Title',
+        ),
+        actions: [
+          IconButton(
+            tooltip: 'Favorite',
+            icon: const Icon(Icons.favorite),
+            onPressed: () {},
+          ),
+          IconButton(
+            tooltip: 'Search',
+            icon: const Icon(Icons.search),
+            onPressed: () {},
+          ),
+          PopupMenuButton<String>(
+            itemBuilder: (context) {
+              return [
+                PopupMenuItem(
+                  child: Text('First'),
+                ),
+                PopupMenuItem(
+                  child: Text('Second'),
+                ),
+                PopupMenuItem(
+                  child: Text('Third'),
+                ),
+              ];
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.blue,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.comment),
+            label: 'Comentários',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.event),
+            label: 'Agenda',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: 'Conta',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Color.fromARGB(255, 212, 237, 237),
+        onTap: _onItemTapped,
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Column(
+                children: [
+                  CircleAvatar(
+                    radius: 50,
+                    backgroundImage: AssetImage('assets/images/profile.jpg'),
+                  ),
+                  Text(
+                    'Profile',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Home'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => HomePage()),
+                );
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SettingsScreen()),
+                );
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.info),
+              title: Text('About'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => AboutPage()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
